78da8afa27e63f16dbbeb6dedf33a14bfc8a3548
Maria Sut <mariasut17@gmail.com>
2017-11-28 21:01:25 +0100

    rails mikinimous crud exepting delete

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,29 @@
 Rails.application.routes.draw do
+  #read all tasks
+
+root 'articles#index'
+get 'articles', to: 'articles#index'
+
+
+get 'articles/new', to: 'articles#new'
+
+#read just one task
+get 'articles/:id', to: 'articles#show', as: 'article'
+
+
+#create
+
+post 'articles', to: 'articles#create'
+
+#update
+
+get 'articles/:id/edit', to: 'articles#edit', as: 'edit'
+patch 'articles/:id', to: 'articles#update'
+
+
+#delete
+
+delete 'articles/:id', to: 'articles#destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+  require 'faker'
+
+ Article.destroy_all
+
+
+
+10.times do
+  article = Article.new(title: Faker::Book.title, content: Faker::Seinfeld.quote )
+
+  puts "#{article.title} saved!" if article.save
+end
+
+puts "Seeding finished!"
